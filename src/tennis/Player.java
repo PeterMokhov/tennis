7ed6d24c8117eed9d9fb0d7f42a5a63695d745be
Peter Mokhov <petermokhov@gmail.com>
2015-06-22 18:33:42 +0200

    added test

---
+++ src/tennis/Player.java
@@ -0,0 +1,16 @@
+package tennis;
+
+public class Player {
+	private String name;
+	
+	public Player(String name){
+		this.name = name;		
+	}
+
+	public String toString() {
+		return "Player [name=" + name + "]";
+	}
+	
+	
+
+}
---
+++ test/tennis/SetTest.java
@@ -0,0 +1,14 @@
+package tennis;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SetTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
