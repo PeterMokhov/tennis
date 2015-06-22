f43b0b50e7049bdac4a77f5d16b4c22cc8634a65
Peter Mokhov <petermokhov@gmail.com>
2015-06-22 18:17:35 +0200

    minor changes

--- src/tennis/Set.java
+++ src/tennis/Set.java
@@ -4,10 +4,28 @@ import java.util.ArrayList;
 
 public class Set {
 	
-	private ArrayList<Score> games;
+	private ArrayList<Score> set;
 	
-	public Set(ArrayList<Score> games){
-		this.games = games;		
+	public Set(ArrayList<Score> set){
+		this.set = set;		
+	}
+	
+	public ArrayList<Score> GetSet(){
+		return set;
+	}
+	
+	public void add(Score gm){
+		if(!set.contains(gm)){
+			set.add(gm);
+		}
+	}
+	
+	/**
+	 * @param gm is a score which fills up a set
+	 * @return boolean if gm is in set
+	 */
+	public boolean contains(Score gm) {
+		return set.contains(gm);
 	}
 
 }
