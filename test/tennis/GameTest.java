470268018d3f447e51f2d858db1ec2b32a362ce2
Peter Mokhov <petermokhov@gmail.com>
2015-06-23 23:00:49 +0200

    some changes to test

--- test/tennis/GameTest.java
+++ test/tennis/GameTest.java
@@ -13,40 +13,43 @@ import static org.mockito.Mockito.verify;
 public class GameTest {
 
     private Game game;
-    private ScoreBoard scoreBoard;
+    private Score score;
 
     @Before
     public void setUp() throws Exception {
-        scoreBoard = mock(ScoreBoard.class);
-        game = new Game(scoreBoard);
+        score = mock(Score.class);
+        game = new Game(score,5);
     }
 
     @Test
-    public void player1ShouldScoreWhenTheFirstCharacterIs1() {
-        game.score("1");
-
-        verify(scoreBoard).player1Scores();
+    public void playerwinsgametest() {
+        score.winsPoint();
+        score.winsPoint();
+        score.winsPoint();
+        score.getPoints();
+        System.out.println(score.getPoints());
+ //       verify(scoreBoard).player1Scores();
     }
 
-    @Test
-    public void player2ShouldScoreWhenTheFirstCharacterIs2() {
-        game.score("2");
-
-        verify(scoreBoard).player2Scores();
-    }
-
-    @Test
-    public void shouldScorePlayer1TwiceWhenThereAreTwo1s() {
-        game.score("11");
-
-        verify(scoreBoard, times(2)).player1Scores();
-    }
-
-    @Test
-    public void shouldScoreBothPlayersWhenThereIsA1AndA2() {
-        game.score("21");
-
-        verify(scoreBoard).player1Scores();
-        verify(scoreBoard).player2Scores();
-    }
+//    @Test
+//    public void player2ShouldScoreWhenTheFirstCharacterIs2() {
+//        game.score("2");
+//
+//        verify(scoreBoard).player2Scores();
+//    }
+//
+//    @Test
+//    public void shouldScorePlayer1TwiceWhenThereAreTwo1s() {
+//        game.score("11");
+//
+//        verify(scoreBoard, times(2)).player1Scores();
+//    }
+//
+//    @Test
+//    public void shouldScoreBothPlayersWhenThereIsA1AndA2() {
+//        game.score("21");
+//
+//        verify(scoreBoard).player1Scores();
+//        verify(scoreBoard).player2Scores();
+//    }
 }
